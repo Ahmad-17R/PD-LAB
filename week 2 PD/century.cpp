f4cbb12348366a5da476c2f9d4f4679a8a2b84bb
MuhammadAhmad69 <ahmadhoney7850@gmail.com>
2023-09-16 08:47:39 +0500

    These are  week 2 tasks

---
+++ week 2 PD/century.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+ main(){
+cout<<"            ......       CCCC    CCCC                ......             "<<endl;
+cout<<"            ......      C    C  CC  CC               ......           "<<endl;
+cout<<"             .....        CCC   CC  CC               .....          "<<endl;
+cout<<"             .....       CC     CC  CC               .....          "<<endl;
+cout<<"              ....    ..CCCCCC...CCCC..........      ....      "<<endl;
+cout<<"              ....        C E N T U R Y              ....         "<<endl;
+cout<<"               ...   ..........................      ...        "<<endl;
+cout<<"               ...   cccccc ccccc c    c  cccccc     ...        "<<endl;
+cout<<"                ..     cc   c      c  c     cc       ..           "<<endl;
+cout<<"               _||_    cc   cccc     c      cc      _||_          "<<endl;
+cout<<"              |    |   cc   c       c  c    cc     |    |     "<<endl;
+cout<<"   ___________|____|___cc   ccccc  c    c   cc  ___|____|__________                                        "<<endl;
+
+
+}
---
+++ week 2 PD/colorpanman.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+  int main(){
+system("color 90");
+cout<<"          ..::---::.."<<endl;
+cout<<"        .---------------."<<endl;
+cout<<"      .-------------------."<<endl;
+cout<<"     -------------------:."<<endl;
+cout<<"   :----------------::."<<endl;
+cout<<"   ----------------:."<<endl;
+cout<<"   :----------------:.."<<endl;
+cout<<"    :-------------------:."<<endl;
+cout<<"     .------------------:"<<endl;
+cout<<"       .:------------:."<<endl;          
+}
