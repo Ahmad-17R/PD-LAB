3cbca9fa6393cc07239f6d53b3f5eefa62e78f0b
MuhammadAhmad69 <144415212+MuhammadAhmad69@users.noreply.github.com>
2023-10-01 08:43:44 +0500

    uams.cpp

--- week 4 PD/uams.cpp
+++ week 4 PD/uams.cpp
@@ -1,69 +1,56 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void menu();
-void compare(float,float);
-void aggregate(float,float, float,string);
-void aggregate2(float,float, float,string);
-void nameaggr()
-main(){
-	menu();
-	aggregate();
-	aggregate2();
-	compare(aggregate,aggregate2);	
 
+void calculateAggregate(string, int, int , int);
+void printMenu();
+void compareMarks(string, int, string, int);
 
-}
-
-void menu(){
-	cout<<"---------------------------<<<<<<<<<<<<<University of Engineering and technology>>>>>>>>>>>>>----------------------"<<endl;
-	cout<<"main menu"<<endl;
-}
-void aggregate(float matric,float inter, float ecat,string name){
+int main() {
 	
-	cout<<"Enter the student's name: ";	
-	cin>>name;
-	cout<<"Enter matriculation marks (out of 1100): ";	
-	cin>>matric;
-	cout<<"Enter intermediate marks (out of 550): ";
-	cin>>inter;	
-	cout<<"Enter Ecat marks (out of 400): ";
-	cin>>ecat;
-	float aggregate;
-	float Matric;
-	float Inter;
-	float Ecat;
-	Matric=0.1*(matric/1100);
-	Inter=0.4*(inter/550);
-	Ecat=0.5*(ecat/400);
-	aggregate=(Matric+Inter+Ecat)*100;
-	cout<<"Aggregate score for "<<name<<" in UET is: "<<aggregate<<"%";}
-void aggregate2(float matric,float inter, float ecat,string name2){
-	
-	cout<<"Enter the student's name: ";	
-	cin>>name2;
-	cout<<"Enter matriculation marks (out of 1100): ";	
-	cin>>matric;
-	cout<<"Enter intermediate marks (out of 550): ";
-	cin>>inter;	
-	cout<<"Enter Ecat marks (out of 400): ";
-	cin>>ecat;
-	float aggregate2;
-	float Matric;
-	float Inter;
-	float Ecat;
-	Matric=0.1*(matric/1100);
-	Inter=0.4*(inter/550);
-	Ecat=0.5*(ecat/400);
-	aggregate2=(Matric+Inter+Ecat)*100;
-	cout<<"Aggregate score for "<<name2<<" in UET is: "<<aggregate2<<"%";}
-void compare(float aggregate,string name,float aggregate2, string name2){
-	if(aggregate>aggregate2){
-	cout<<"1."<<name<<"   "<<aggregate<<endl;
-	cout<<"1."<<name2<<"   "<<aggregate2<<endl;
+        printMenu();
+	string name;
+	float matric,inter,ecat;
+	cout<<"Name: ";
+	cin>> name;
+	cout<<"Matric marks: ";
+	cin>> matric;
+	cout<<"Inter marks: ";
+	cin>> inter;
+	cout<<"Ecat marks: ";
+	cin>> ecat;
+       	calculateAggregate(name,matric,inter,ecat);
+
+	cout<<endl;
+	string nam1,nam2;
+	float marks1,marks2;
+        cout<<"Name of student1: ";
+	cin>> nam1;
+	cout<<"Marks of student1: ";
+	cin>> marks1;
+	cout<<"Name of student2: ";
+	cin>> nam2;
+	cout<<"Marks of student2: ";
+	cin>> marks2;
+        compareMarks(nam1,marks1,nam2,marks2);
+    
 }
-	if(aggregate<aggregate2){
-	cout<<"1."<<name2<<"   "<<aggregate2<<endl;
-	cout<<"1."<<name<<"   "<<aggregate<<endl;
+void printMenu() {
+cout<<"---------------------------<<<<<<<<<<<<<University of Engineering and technology>>>>>>>>>>>>>----------------------"<<endl;
 }
 
+void calculateAggregate(string name, int matricMarks, int interMarks, int ecatMarks)
+{
+	float aggregate = ((matricMarks / 1100.0) * 0.3 + (interMarks / 550.0) * 0.3 + (ecatMarks / 400.0) * 0.4)*100;
+	cout << "Aggregate for " << name << " is: " << aggregate<<"%" <<endl;
+}
+
+void compareMarks(string nameStudent1, int ecatMarksStudent1, string nameStudent2, int ecatMarksStudent2) 
+{
+        if (ecatMarksStudent1 > ecatMarksStudent2) {
+        cout << nameStudent1 << "is alotted first roll no. "<<endl;
+        }  
+        if (ecatMarksStudent2 > ecatMarksStudent1) {
+        cout << nameStudent2 << " is alotted first roll no. "<<endl;
+   	}  
+	
 }
